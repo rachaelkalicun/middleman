13c4f4ae322df3b262ae94de2903a2e1379280ed
Rachael <rhg108@gmail.com>
2025-02-24 11:30:34 -0700

    try adding logger first

--- middleman-core/lib/middleman-core/util.rb
+++ middleman-core/lib/middleman-core/util.rb
@@ -1,5 +1,6 @@
+require "logger"
 require "active_support/version"
-require "logger" if ActiveSupport.version < Gem::Version.new("8.0.0")
+# require "logger" if ActiveSupport.version < Gem::Version.new("8.0.0")
 
 require 'active_support/all'
 
